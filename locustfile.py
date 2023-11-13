c52b9a84fc69c57e4b71cb1f5bd6202009aee7dd
rpizziol <roberto.pizziol@imtlucca.it>
2023-11-13 15:13:50 +0100

    Commented attempt to automatize workload

--- locustfile.py
+++ locustfile.py
@@ -1,21 +1,51 @@
 import random
-from locust import HttpUser, task
+from locust import HttpUser, task, events, constant
 
 
 class MyUser(HttpUser):
     base_url = "http://192.168.1.100:5000"  # URL of the web application
 
+    wait_time = random.randint(100, 500)  # Simulate random wait time between requests
+
     @task
     def index_page(self):
         self.client.get("/")
 
-    @task
+    @task(weight=2)
     def product_page(self):
         self.client.get("/product")
 
-    @task
+    @task(weight=1)
     def checkout_page(self):
         self.client.get("/checkout")
 
 
-wait_time = random.randint(100, 500)  # Simulate random wait time between requests
+# # Maximum number of users
+# max_users = 100
+#
+# # Rate at which new users are created (per second)
+# spawn_rate = 10
+#
+# # Total run time of the test in seconds
+# run_time = 300
+#
+#
+# # Configure the load test to start with 10 users and gradually increase to the maximum number of users
+# def on_start(environment):
+#     environment.user_count += 1
+#     if environment.user_count < max_users:
+#         events.locust_user_spawned.fire(environment, None, 1)
+#     else:
+#         timer = events.timer.Timer(spawn_rate, environment.user_count)
+#         timer.add_listener(on_timer_fire)
+#         timer.start()
+#
+#
+# def on_timer_fire(timer):
+#     if timer.remaining_time <= 0:
+#         timer.stop()
+#         return
+#     if timer._current_count < max_users:
+#         events.locust_user_spawned.fire(timer.environment, None, 1)
+#         timer._current_count += 1
+#     timer.reschedule()
