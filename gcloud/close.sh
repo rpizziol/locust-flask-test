f7528307a8489da659f832c6ba9a2bab5c52dbda
rpizziol <roberto.pizziol@gmail.com>
2023-12-20 10:27:13 +0100

    Close Kubernetes script

---
+++ gcloud/close.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Kill all the Kubernetes cluster
+echo +++ DELETING THE CLUSTERS +++
+python3 gcloud_script.py del
+
+# Play a sound to notify end
+echo +++ TASK FINISHED +++
+aplay /usr/share/sounds/sound-icons/at &> /dev/null
