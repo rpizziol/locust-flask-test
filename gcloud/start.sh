2aabcf88cac6745aa9b832507758087a400507b3
rpizziol <roberto.pizziol@gmail.com>
2023-12-20 09:35:17 +0100

    python > python3 and change of sound

--- gcloud/start.sh
+++ gcloud/start.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 # Generate Kubernetes cluster
-python gcloud_script.py new
+echo +++ CREATING THE CLUSTER +++
+python3 gcloud_script.py new
 
 # Deploy the application on the cluster
-python gcloud_script.py kub
+echo +++ DEPLOYING THE APPLICATION TO THE CLUSTER +++
+python3 gcloud_script.py kub
 
 # Play a sound to notify end
-paplay /usr/share/sounds/freedesktop/stereo/complete.oga 
+echo +++ TASK FINISHED +++
+aplay /usr/share/sounds/sound-icons/at &> /dev/null
 
 # Print the assigned IP (required then for locust)
 kubectl get service my-prime-checker-app-service
