71909999bb11e1615634a1b4061dd4c437f63f43
rpizziol <roberto.pizziol@gmail.com>
2024-04-15 11:20:46 +0200

    Replaced actual traceShap file

--- locust/traceShape.py
+++ locust/traceShape.py
@@ -1,11 +1,9 @@
+import csv
 from locust import LoadTestShape
-import subprocess
-import math
-import numpy as np
-from scipy.io import loadmat
+import pandas as pd
 
 
-class traceShape(LoadTestShape):
+class TraceShape(LoadTestShape):
     lastStage = None
     mod = None
     shift = None
@@ -13,14 +11,13 @@ class traceShape(LoadTestShape):
     traceFile = None
     data = None
 
-    def __init__(self, mod=200, shift=10, duration=300, traceFile="trace.mat"):
+    def __init__(self, mod=400, shift=10, duration=600, traceFile="./workloads/trace.csv"):
         super().__init__()
         self.mod = mod
         self.shift = shift
         self.duration = duration
         self.traceFile = traceFile
-        # load trace data (for now in matlab format but in general I have to use a csv)
-        self.data = loadmat(f"{self.traceFile}")["users"][0]
+        self.data = pd.read_csv(self.traceFile).to_numpy().T[0]
         self.maxIndex = len(self.data)
         # rescale the trace
         mx = max(self.data)
@@ -30,10 +27,17 @@ class traceShape(LoadTestShape):
     def tick(self):
         run_time = self.get_run_time()
         if run_time <= self.duration:
-            if (int(run_time) % 60 == 0):
+            if int(run_time) % 20 == 0:  # Update number of users every 20 secs
                 self.users = (self.f(int(run_time)), 1)
+                self.save_users(self.users)
             return self.users
         return None
 
     def f(self, x):
         return self.data[int(x) % self.maxIndex]
+
+    def save_users(self, users):
+        with open("./workload.csv", mode='a', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow([users])
+            print(f"Saved users: {users}")
