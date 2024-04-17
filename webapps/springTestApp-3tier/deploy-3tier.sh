9bae7da9b8306763fce26509f8a83a8383df3a07
rpizziol <roberto.pizziol@gmail.com>
2024-04-17 10:50:27 +0200

    Webapps code restructuring

--- webapps/springTestApp-3tier/deploy-3tier.sh
+++ webapps/springTestApp-3tier/deploy-3tier.sh
@@ -5,14 +5,14 @@ kubectl apply -f tier3-deployment.yaml
 sleep 2
 kubectl apply -f tier3-service.yaml
 
-sleep 3
+sleep 2
 
 # Tier 2
 kubectl apply -f tier2-deployment.yaml
 sleep 2
 kubectl apply -f tier2-service.yaml
 
-sleep 3
+sleep 2
 
 # Tier 1
 kubectl apply -f tier1-deployment.yaml
@@ -20,7 +20,7 @@ sleep 2
 kubectl apply -f tier1-service.yaml
 
 # Deploy HPA on each tier
-# kubectl autoscale deployment spring-test-app-tier1 --cpu-percent=50 --min=1 --max=100
+ kubectl autoscale deployment spring-test-app-tier1 --cpu-percent=50 --min=1 --max=100
 for i in {1..3}; do kubectl autoscale deployment "spring-test-app-tier$i" --cpu-percent=50 --min=1 --max=100; done
 
 # Check if HPA was correctly deployed
